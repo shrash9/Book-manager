96c1ad926805de0788b837ad67acde4b604763b5
shrash9 <shrasti.20225084@mnnit.ac.in>
2025-04-23 10:35:05 +0530

    Added new effects

--- src/app/books/book.service.ts
+++ src/app/books/book.service.ts
@@ -10,6 +10,8 @@ export class BookService {
   constructor() { }
 
   addBook(book:Book): Observable<Book>{
+    const err = new Error('Error while adding a book')
+    return throwError(() => err)
     return of(book);
   }
 }
